87ef8ada25ab080eae50e714b1b9768e26939c3b
glazov <glazov92@gmail.com>
2022-11-05 13:52:53 +0300

    добавили else в возраст

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const age = 15;
+const age = 18;
 const gender = "жЕн";
 
 if (age > 18) {
   console.log("Вам больше 18 лет");
+} else {
+  console.log("Вам меньше 18 лет");
 }
 
 if (gender.toLocaleLowerCase() === "муж") {
