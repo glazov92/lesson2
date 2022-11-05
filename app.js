f454b735b5ebe4d92e9618fed8dfa19612587498
glazov <glazov92@gmail.com>
2022-11-05 13:48:57 +0300

    Условия if

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Привет');
+const age = 15;
+const gender = "жЕн";
+
+if (age > 18) {
+  console.log("Вам больше 18 лет");
+}
+
+if (gender.toLocaleLowerCase() === "муж") {
+  console.log("Вам налево");
+}
+if (gender.toLocaleLowerCase() === "жен") {
+  console.log("Вам направо");
+}
